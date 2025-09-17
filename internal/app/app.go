b6368526e08bd13864b0084422fa8ff546f291b2
Henge <18427186+Henge9@users.noreply.github.com>
2025-09-17 21:36:06 +0200

    first commit

---
+++ internal/app/app.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Henge9/femProject/internal/api"
+)
+
+type Application struct {
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
+}
+
+func NewApplication() (*Application, error) {
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	// our stores go here
+
+	// our handlers go here
+	workoutHandler := api.NewWorkoutHandler()
+
+	app := &Application{
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
+	}
+
+	return app, nil
+}
+
+func (a *Application) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
+}
